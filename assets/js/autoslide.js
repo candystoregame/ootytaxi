c94d77982683d90d17dab7a4b666f08ecec21919
anjancgfbb@gmail.com <anjancgfbb@gmail.com>
2022-10-25 20:42:38 +0530

    second

---
+++ assets/js/autoslide.js
@@ -0,0 +1,14 @@
+var myIndex = 0;
+function myslider() {
+    var i;
+    var x = document.getElementsByClassName("home1-testm item");
+    for (i=0; i < x.length; i++) {
+      x[i].style.display;/* = "none";*/
+    }
+myIndex++;
+if (myIndex > x.length) { myIndex = 1}
+x[myIndex-1].style.display = "block";
+x[myIndex].style.display = "block";
+x[myIndex+1].style.display = "block";
+setTimeout(myslider, 10);
+}myslider();
