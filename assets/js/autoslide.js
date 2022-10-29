27e8525b75666a8e2bacb7363b92371ea5b705bf
anjancgfbb@gmail.com <anjancgfbb@gmail.com>
2022-10-29 11:28:04 +0530

    booknow

--- assets/js/autoslide.js
+++ assets/js/autoslide.js
@@ -1,9 +1,9 @@
 var myIndex = 0;
 function myslider() {
     var i;
-    var x = document.getElementsByClassName("home1-testm item");
+    var x = document.getElementsByClassName("home1-testm");
     for (i=0; i < x.length; i++) {
-      x[i].style.display = "none";
+      x[i].style.display;/* = "none";*/
     }
 myIndex++;
 if (myIndex > x.length) { myIndex = 1}
@@ -12,6 +12,3 @@ x[myIndex].style.display = "block";
 x[myIndex+1].style.display = "block";
 setTimeout(myslider, 5);
 }myslider();
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
