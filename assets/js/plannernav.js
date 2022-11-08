004f8e0e7e7930fa2e4235ad266e2b008ee32419
anjancgfbb@gmail.com <anjancgfbb@gmail.com>
2022-11-08 11:03:18 +0530

    midmenu

--- assets/js/plannernav.js
+++ assets/js/plannernav.js
@@ -1,3 +1,36 @@
 function fetchElement(value) {
-    alert(value);
-  }
+  fetchMidMenu(value);
+}
+
+function fetchMidMenu(fileName)
+{
+  let newDiv = document.createElement("div");
+  fName = './assets/dataFiles/' + fileName;
+  console.log(fileName);
+  let tablecon = document.querySelector('.plan-midmenu');
+    (
+      async() => {
+        var text;
+        let workbook = XLSX.read(await (await fetch(fName)).arrayBuffer(), {type: 'array'});
+        //let worksheet = workbook.SheetNames;
+        let html = XLSX.utils.sheet_to_json(workbook.Sheets['Sheet1']);
+        console.log(html);
+        for (var key in html) {
+            for (var header in html[key]) {
+                text = html[key][header];
+                console.log(text);
+                var _button = document.createElement("button");
+                _button.classList.add("btn-plan-submenu");
+                _button.value = text;
+                _button.onclick = function() { alert(this.value); };
+                _button.innerHTML = text;
+            }
+            newDiv.classList.add("plan-btn-pane")
+            newDiv.appendChild(_button);
+            tablecon.appendChild(newDiv);
+
+
+        }
+      }
+    )()
+}
