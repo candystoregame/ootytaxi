955ff45d46a5e1edb42f033507cdd5d634760e8c
anjancgfbb@gmail.com <anjancgfbb@gmail.com>
2022-11-11 03:17:04 +0530

    PerfectExtract

--- assets/js/plannernav.js
+++ assets/js/plannernav.js
@@ -11,27 +11,29 @@ function fetchMidMenu(fileName)
   tablecon.innerHTML='';
     (
       async() => {
-        var text;
+        let text, _button, counter = 0, token;
         let workbook = XLSX.read(await (await fetch(fName)).arrayBuffer(), {type: 'array'});
-        //let worksheet = workbook.SheetNames;
-        let html = XLSX.utils.sheet_to_json(workbook.Sheets['Sheet1']);
+        let worksheet = workbook.SheetNames;
+        let html = XLSX.utils.sheet_to_json(workbook.Sheets[worksheet[0]]);
         console.log(html);
-        for (var key in html) {
-            for (var header in html[key]) {
-                text = html[key][header];
-                //console.log(text);
-                var _button = document.createElement("button");
-                _button.classList.add("btn-plan-submenu");
-                _button.value = text;
-                _button.type = 'button';
-                _button.onclick = function() { alert(this.value); };
-                _button.innerHTML = text;
-            }
-            newDiv.classList.add("plan-btn-pane")
-            newDiv.appendChild(_button);
-            tablecon.appendChild(newDiv);
-
-
+        for (let key in html) {
+          for (let header in html[key]) {
+            text = html[key][header];
+            console.log(header);
+            token = header;
+            _button = document.createElement("button");
+            _button.type = 'button';
+            _button.classList.add("btn-plan-submenu");
+            //_button.value = text;
+            _button.value = 'Sheet' + (2+counter)
+            _button.onclick = function() { alert(this.value); };
+            _button.innerHTML = text;
+          }
+          newDiv.classList.add("plan-btn-pane");
+          if (counter == 0) { let hEader = document.createElement("h3"); hEader.innerHTML=token; newDiv.appendChild(hEader); }
+          newDiv.appendChild(_button);
+          tablecon.appendChild(newDiv);
+          counter = counter+1;
         }
       }
     )()
