fe088000c17ae3b0cfafbfc2faf3b4b176b508f0
anjancgfbb@gmail.com <anjancgfbb@gmail.com>
2022-11-30 20:04:36 +0530

    Modify Everything start

--- assets/js/plannernav.js
+++ assets/js/plannernav.js
@@ -195,25 +195,27 @@ function fetchMidMenu(fileName)
         const workbook = XLSX.read(await (await fetch(fName)).arrayBuffer(), {type: 'array'});
         const worksheet = workbook.SheetNames;
         const html = XLSX.utils.sheet_to_json(workbook.Sheets[worksheet[0]]);
-        //console.log(html);
-        for (let key in html) {
-          for (let header in html[key]) {
-            text = html[key][header];
-            token = header;
-            _button = document.createElement("button");
-            _button.type = 'button';
-            _button.id = 'btn-plan-submenu-id' + (counter + 1);
-            if (counter == 0) {_button.classList.add("btn-plan-submenu"); _button.className += ' active';}
-            else {_button.classList.add("btn-plan-submenu");}
-            _button.value = 'Sheet' + (2+counter)
-            _button.onclick = function() { rsma(); fetchTable (fName, this.value); this.classList.toggle('active');};
-            _button.innerHTML = text;
+        console.log(worksheet.length);
+        if(worksheet.length > 2) {
+          for (let key in html) {
+            for (let header in html[key]) {
+              text = html[key][header];
+              token = header;
+              _button = document.createElement("button");
+              _button.type = 'button';
+              _button.id = 'btn-plan-submenu-id' + (counter + 1);
+              if (counter == 0) {_button.classList.add("btn-plan-submenu"); _button.className += ' active';}
+              else {_button.classList.add("btn-plan-submenu");}
+              _button.value = 'Sheet' + (2+counter)
+              _button.onclick = function() { rsma(); fetchTable (fName, this.value); this.classList.toggle('active');};
+              _button.innerHTML = text;
+            }
+            newDiv.classList.add("plan-btn-pane");
+            if (counter == 0) { let hEader = document.createElement("h3"); hEader.innerHTML=token; newDiv.appendChild(hEader); }
+            newDiv.appendChild(_button);
+            midMenu.appendChild(newDiv);
+            counter = counter+1;
           }
-          newDiv.classList.add("plan-btn-pane");
-          if (counter == 0) { let hEader = document.createElement("h3"); hEader.innerHTML=token; newDiv.appendChild(hEader); }
-          newDiv.appendChild(_button);
-          midMenu.appendChild(newDiv);
-          counter = counter+1;
         }
       }
     )()
