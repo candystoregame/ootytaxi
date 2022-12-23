776409fb811fd60b3766e193f1c331ff6fb3bdf3
candystoregame <anjancgfbb@gmail.com>
2022-12-24 02:39:40 +0530

    animation added to table

--- assets/js/plannernav.js
+++ assets/js/plannernav.js
@@ -15,6 +15,24 @@ const keynotemap = new Map([
   ["Honeymoon Tour Packages.xlsx", "tariffnote5"]
 ]);
 
+const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+  });
+
 let planner_booknow_map = [];
 
 let defaultmenu = 'tariff-planner1';
@@ -59,10 +77,10 @@ function fetchTable(fetchMidMenu, sheetName) {
   const fName = fetchMidMenu;
   planner_booknow_map = [];
   let buttonmap,indext =0;
-  let plname, veh, freekm, freehrs, xhr, xkm, pamount;
   if (globaltableflag != 1) {
     rightMainDiv.innerHTML='';
   }
+  animateCSS('.table-right-content', 'jackInTheBox');
   tableContent.innerHTML='';
   (
     async() => {
