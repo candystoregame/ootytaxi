a96b399dbe80f7910707a6ba3d18804ab75acfc4
candystoregame <anjancgfbb@gmail.com>
2023-04-12 23:41:25 +0530

    slider quotes updated

--- assets/js/mycarousel.js
+++ assets/js/mycarousel.js
@@ -10,7 +10,7 @@ function clickslider(value) {
     const navtrack = document.querySelector('.slider-navigation');
     let sliderpivot = 1;
     const cslidedots = Array.from(document.querySelectorAll('.slider-btn'));
-    let timer = setInterval(autoSlide, 3000);
+    let timer = setInterval(autoSlide, 10000);
     navtrack.addEventListener('click', e => {
       const targetdot = e.target.closest("div.slider-btn");
       if (!targetdot) return;
@@ -25,7 +25,7 @@ function clickslider(value) {
 
     function resetTimer() {
       clearInterval(timer);
-      timer = setInterval(autoSlide, 5000);
+      timer = setInterval(autoSlide, 10000);
     }
   
     function autoSlide() {
@@ -47,7 +47,7 @@ function clickslider(value) {
       cslides[sliderpivot - 1].classList.add("active");
       cslidedots[sliderpivot - 1].classList.add("active");
     }
-  }, 3000);
+  }, 2000);
 
   closepopslider.addEventListener('click', function() {
       popslider.style.display = 'none';
@@ -61,78 +61,3 @@ function clickslider(value) {
       }
     })
   }
-
-  /*function clickslider(value) {
-    let slidertoken = value;
-    let quitslider;
-    let timer;
-    let sliderpivot = 1;
-  
-    fetchslidecontent(slidertoken);
-    const popslider = document.getElementById('ootyslider-popup');
-    const closepopslider = document.querySelector('.close-ootyslider-pop');
-    popslider.style.display = 'block';
-  
-    const slidetrack = document.querySelector('.img-slider');
-    const cslides = Array.from(document.querySelectorAll('.ooty-slide'));
-    const navtrack = document.querySelector('.slider-navigation');
-    const cslidedots = Array.from(document.querySelectorAll('.slider-btn'));
-  
-    function resetTimer() {
-      clearInterval(timer);
-      timer = setInterval(autoSlide, 5000);
-    }
-  
-    function autoSlide() {
-      if (slidertoken === null) {
-        clearInterval(timer);
-        clearInterval(quitslider);
-      }
-      sliderpivot += 1;
-      slidefun(sliderpivot);
-    }
-  
-    function slidefun(n) {
-      if (slidertoken === null) {
-        clearInterval(timer);
-        clearInterval(quitslider);
-      }
-      slidetrack.querySelector('.active').classList.remove('active');
-      navtrack.querySelector('.active').classList.remove('active');
-      if (n > cslides.length) {
-        sliderpivot = 1;
-      }
-      if (n < 1) {
-        sliderpivot = myslide.length;
-      }
-      cslides[sliderpivot - 1].classList.add('active');
-      cslidedots[sliderpivot - 1].classList.add('active');
-    }
-  
-    navtrack.addEventListener('click', (e) => {
-      const targetdot = e.target.closest('div.slider-btn');
-      if (!targetdot) return;
-      const targetIndex = cslidedots.findIndex((dot) => dot === targetdot);
-      slidetrack.querySelector('.active').classList.remove('active');
-      navtrack.querySelector('.active').classList.remove('active');
-      cslides[targetIndex].classList.add('active');
-      cslidedots[targetIndex].classList.add('active');
-      sliderpivot = targetIndex;
-      resetTimer();
-    });
-  
-    quitslider = setTimeout(() => {
-      timer = setInterval(autoSlide, 3000);
-    }, 3000);
-
-    closepopslider.addEventListener('click', () => {
-      popslider.style.display = 'none';
-      slidertoken = null;
-    })
-    window.addEventListener('click', e => {
-      if (e.target === popslider) {
-        popslider.style.display = 'none';
-        slidertoken = null;
-      }
-    })
-  } */
